530a3471782f7260a4dada50d810decc722e7ec3
atelier-de-fable <322551402+atelier-de-fable@users.noreply.github.com>
2026-09-07 00:36:19 +0200

    Publier le jeu actualisé et le dashboard de consultation

---
+++ data/scenes-officiers.js
@@ -0,0 +1,1 @@
+window.LEST=window.LEST||{},LEST.SCENES_OFFICIERS={bosco:"assets/conseiller-bosco.png",maitre:"assets/conseiller-maitre.png",pilote:"assets/conseiller-pilote.png"};
---
+++ moteur/arrivee.js
@@ -0,0 +1,1 @@
+window.LEST=window.LEST||{},LEST.ouvrirArrivee=function(e){const i=LEST.ARRIVEE;return!e.faits.aEuLieu("message-force",{message:i.id})&&(LEST.Evenements.appliquer(e,i),!0)};
